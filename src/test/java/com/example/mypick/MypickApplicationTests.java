2f66a1d0f7ea1418f094b160f18022658729edd9
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-06-10 14:59:26 +0900

    Initial commit
    개발 환경 설정

---
+++ src/test/java/com/example/mypick/MypickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
